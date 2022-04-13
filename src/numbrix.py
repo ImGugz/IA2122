f08f8d9b2435fbbfbaed2e4dbe9267c5d83e1ca5
ImGugz <gustavoaaguiar@tecnico.ulisboa.pt>
2022-04-13 14:49:02 +0100

    preliminary inefficient solution

--- src/numbrix.py
+++ src/numbrix.py
@@ -5,18 +5,12 @@
 # Grupo 20:
 # 95565 Duarte Almeida
 # 95587 Gustavo Aguiar
-
+import copy
 import sys
+import time
+
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, \
     recursive_best_first_search
-
-# Auxiliary Functions
-
-def is_square(matrix):
-    """ Auxiliary function that receives a matrix and returns true if it's squared, and false otherwise. """
-    return all(len(row) == len(matrix) for row in matrix)
-
-# --------------------
 
 class NumbrixState:
     state_id = 0
@@ -37,14 +31,33 @@ class Board:
 
     board_size = 0
     board = []
-
-    def __init__(self, board_size, board):
-        self.board_size = board_size
-        self.board = board
+    numbers_to_go = set()
+    free_adjacents = set()
+    vertical_adjacents = 0
+    horizontal_adjacents = 0
+    # board_size, board, initial_numbers
+    def __init__(self, *args):
+        self.board = args[0]
+        self.board_size = args[1]
+        if len(args) == 3:
+            self.numbers_to_go = {i for i in range(1, self.board_size * self.board_size + 1)} - args[2]
+            self.free_adjacents = set([(i, j) for j in range(self.board_size) for i in range(self.board_size)
+                                       if self.has_adjacents(i, j)])
+            for i in range(self.board_size):
+                self.horizontal_adjacents += sum(
+                    [1 if ((self.board[i][j] == self.board[i][j + 1] + 1 or self.board[i][j] == self.board[i][j + 1] - 1) and self.board[i][j] != 1) else 0 for j in
+                     range(self.board_size - 1)])
+                self.vertical_adjacents += sum(
+                    [1 if ((self.board[j][i] == self.board[j + 1][i] + 1 or self.board[j][i] == self.board[j + 1][i] - 1) and self.board[i][j] != 1) else 0 for j in
+                     range(self.board_size - 1)])
+        else:
+            self.numbers_to_go = args[2]
+            self.free_adjacents = args[3]
+            self.vertical_adjacents = args[4]
+            self.horizontal_adjacents = args[5]
 
     def get_number(self, row: int, col: int) -> int:
         """ Devolve o valor na respetiva posição do tabuleiro. """
-
         try:
             return self.board[row][col]
         except IndexError:
@@ -53,87 +66,116 @@ class Board:
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente abaixo e acima,
         respectivamente. """
-
-        result = []
-        for direction in [1, -1]:
+        results = []
+        for dir in [1, -1]:
             try:
-                result.append(self.board[row + direction][col])
+                results.append(self.board[row + dir][col])
             except IndexError:
-                result.append(None)
-        return tuple(result)
+                results.append(None)
+        return tuple(results)
 
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente à esquerda e à direita,
         respectivamente. """
-
-        result = []
-        for direction in [-1, 1]:
+        results = []
+        for dir in [-1, 1]:
             try:
-                result.append(self.board[row][col + direction])
+                results.append(self.board[row][col + dir])
             except IndexError:
-                result.append(None)
-        return tuple(result)
+                results.append(None)
+        return tuple(results)
 
     @staticmethod
     def parse_instance(filename: str):
         """ Lê o ficheiro cujo caminho é passado como argumento e retorna
         uma instância da classe Board. """
-
         file_board = []
         try:
             with open(filename) as f:
-                board_size = f.readline()
+                initial_numbers = set()
+                board_size = int(f.readline())
                 for line in f.readlines():
                     split = line.split('\t')
+                    [initial_numbers.add(int(i)) for i in split if int(i) != 0]
                     file_board.append([int(i) for i in split])
-            if not is_square(file_board):
-                raise ValueError
-            return Board(board_size, file_board)
+            return Board(file_board, board_size, initial_numbers)
         except IOError:  # Couldn't open input file
             print("Something went wrong while attempting to read file.")
-            sys.exit(-1)
-        except ValueError:  # Invalid input board
-            print("The given matrix isn't square (NxN).")
-            f.close()
             sys.exit(-1)
 
     # TODO: outros metodos da classe
 
     def to_string(self):
-        return '\n'.join('\t'.join(f'{i}' for i in x) for x in self.board)
+        return '\n'.join('\t'.join(f'{i}' for i in x) for x in self.board) + '\n'
 
+    def get_adjacents(self, row, col):
+        return list(filter(lambda x: 0 <= x[0] < self.board_size and 0 <= x[1] < self.board_size
+                           , [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]))
+
+    def has_adjacents(self, row, col):
+        return (0 in self.adjacent_vertical_numbers(row, col) or 0
+            in self.adjacent_horizontal_numbers(row, col)) and self.get_number(row, col) != 0
+
+    def apply_action(self, action):
+        (row, col, value) = action
+        new_board = copy.deepcopy(self)
+        new_board.board[row][col] = value
+        new_board.numbers_to_go = new_board.numbers_to_go - {value}
+        new_board.free_adjacents = new_board.free_adjacents - set(new_board.get_adjacents(row, col))
+        new_board.free_adjacents = new_board.free_adjacents.union(set(el for el in new_board.get_adjacents(row, col) + [(row, col)]
+                                   if new_board.has_adjacents(el[0], el[1])))
+        new_board.vertical_adjacents = new_board.vertical_adjacents + sum([1 if j == col and abs(new_board.board[row][col] - new_board.board[i][j]) == 1 else 0
+                                        for i, j in new_board.get_adjacents(row, col)])
+        new_board.horizontal_adjacents = new_board.horizontal_adjacents + sum([1 if i == row and abs(new_board.board[row][col] - new_board.board[i][j]) == 1 else 0
+                                          for i, j in new_board.get_adjacents(row, col)])
+        return NumbrixState(new_board)
 
 class Numbrix(Problem):
     def __init__(self, board: Board):
         """ O construtor especifica o estado inicial. """
-        # TODO
-        pass
+        self.initial = NumbrixState(board)
 
     def actions(self, state: NumbrixState):
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
-        # TODO
-        pass
+        actions = []
+        for (row, col) in state.board.free_adjacents:
+            adj_value = state.board.get_number(row, col)
+            adjacents = state.board.get_adjacents(row, col)
+            for (adj_r, adj_c) in adjacents:
+                if state.board.get_number(adj_r, adj_c) != 0:
+                    continue
+                adj_vector = (adj_r - row + adj_r, adj_c - col + adj_c)
+                aux_actions = [-1, 1]
+                if (0 <= adj_vector[0] < state.board.board_size) and (0 <= adj_vector[1] < state.board.board_size):
+                    adj_vec_num = state.board.get_number(adj_vector[0], adj_vector[1])
+                    if adj_vec_num != 0 and adj_vec_num - adj_value != 2:
+                        aux_actions.remove(1)
+                    if adj_vec_num != 0 and adj_vec_num - adj_value != -2:
+                        aux_actions.remove(-1)
+                for action in aux_actions:
+                    if adj_value + action in state.board.numbers_to_go:
+                        actions.append((adj_r, adj_c, adj_value + action))
+        return actions
 
     def result(self, state: NumbrixState, action):
         """ Retorna o estado resultante de executar a 'action' sobre
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state). """
-        # TODO
-        pass
+        return state.board.apply_action(action)
 
     def goal_test(self, state: NumbrixState):
         """ Retorna True se e só se o estado passado como argumento é
         um estado objetivo. Deve verificar se todas as posições do tabuleiro
         estão preenchidas com uma sequência de números adjacentes. """
-        # TODO
-        pass
+        return (state.board.horizontal_adjacents == state.board.board_size * (state.board.board_size - 1)) \
+               or (state.board.vertical_adjacents == state.board.board_size * (state.board.board_size - 1))
 
     def h(self, node: Node):
         """ Função heuristica utilizada para a procura A*. """
-        # TODO
-        pass
+        return node.state.board.board_size * (node.state.board.board_size - 1) \
+               - max(node.state.board.horizontal_adjacents, node.state.board.vertical_adjacents)
 
     # TODO: outros metodos da classe
 
@@ -144,4 +186,13 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
-    pass
+
+    tic = time.perf_counter()
+    board = Board.parse_instance(sys.argv[1])
+    problem = Numbrix(board)
+    goal_node = recursive_best_first_search(problem)
+    toc = time.perf_counter()
+
+    print(f"RBFS: Programa executado em {toc - tic:0.4f} segundos.")
+    print(goal_node.state.board.to_string(), sep="")
+
